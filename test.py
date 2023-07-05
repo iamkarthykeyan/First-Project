ee83a0d90ff63ab45586dbc717a8e7ab0eebf695
Karthikeyan J <karthikeyan02092003@gmail.com>
2023-07-05 19:01:32 +0530

    somes to be done in the testing branches

--- test.py
+++ test.py
@@ -14,5 +14,11 @@ class TestSum(unittest.TestCase):
         result = sum(data)
         self.assertEqual(result, 6.0)
 
-if __name__ == "__main__":
-    unittest.main()
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
