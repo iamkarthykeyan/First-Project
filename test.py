d83075443adb4756a1080eaf7cbb0c650dd7e437
Karthikeyan J <karthikeyan02092003@gmail.com>
2023-07-05 18:43:21 +0530

    checkout the testing branch

---
+++ test.py
@@ -0,0 +1,18 @@
+#Testing codes for app
+
+import unittest
+
+class TestSum(unittest.TestCase):
+
+    def test_list_int(self):
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_float(self):
+        data = [1.0, 2.0, 3.0]
+        result = sum(data)
+        self.assertEqual(result, 6.0)
+
+if __name__ == "__main__":
+    unittest.main()
